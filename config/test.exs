5fc5b1d920bd739e49e122916a3d6cd77f19e78b
Jan Christophersen <code@chrstphrsn.cc>
2020-06-29 19:00:46 +0200

    Update config

--- config/test.exs
+++ config/test.exs
@@ -9,7 +9,7 @@ config :sthub, StHub.Repo,
   username: "postgres",
   password: "postgres",
   database: "sthub_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
+  hostname: System.get_env("POSTGRES_HOST") || "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
